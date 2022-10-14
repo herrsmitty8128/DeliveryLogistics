709937c243c38acf12fa907060eb3687ccfa9525
herrsmitty8128 <16559135+herrsmitty8128@users.noreply.github.com>
2022-10-14 06:05:25 -0400

    updates 2022-10-14 0605

--- test.py
+++ test.py
@@ -1,35 +1,72 @@
 
 import DeliveryLogistics
 import csv
-import json
+import sys
 
+# default values
 avg_unload_secs = 11
 big_truck_payload = 330
-customer_file = 'data_files/2022_orders.csv'
-api_key_file = 'data_files/google_api_key.json'
-distributionCenters = set([DeliveryLogistics.DistributionCenter('Dogwood Elementary School', '12300 Glade Dr, Reston, VA 20191, USA')])
+#customer_file = 'data_files/2022_orders.csv'
+#api_key_file = 'data_files/google_api_key.json'
+#distributionCenters = set([DeliveryLogistics.DistributionCenter('Dogwood Elementary School', '12300 Glade Dr, Reston, VA 20191, USA')])
     
 
 if __name__ == "__main__":
 
-    # load our list of customers
-    customer_orders = set()
-    with open(customer_file,'rt',newline='') as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            customer_orders.add(DeliveryLogistics.DeliveryLocation('',row['address'],row['bags']))
+    if '-from_google' in sys.argv:
+
+        # get the distribution center passed in by the user
+        if '-d' in sys.argv:
+            i = sys.argv.index('-d')
+            n = sys.argv[i+1]
+            d = sys.argv[i+2]
+            distributionCenters = set([DeliveryLogistics.DistributionCenter(n, d)])
+        else:
+            raise ValueError('Expected "-d [distribution center name] [distribution center address]" as command line parameters.')
+
+        # load our list of customers
+        if '-c' in sys.argv:
+            i = sys.argv.index('-c')
+            f = sys.argv[i+1]
+            customer_orders = set()
+            with open(f,'rt',newline='') as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                    customer_orders.add(DeliveryLogistics.DeliveryLocation('',row['address'],row['bags']))
+        else:
+            raise ValueError('Expected -c [sales orders file path and name] as a command line paramter.')
+        
+        # get the google API key
+        if '-k' in sys.argv:
+            i = sys.argv.index('-k')
+            google_api_key = sys.argv[i+1]
+        else:
+            raise ValueError('Expected -k [google key] as a command line parameter.')
+        
+        if '-max_payload' in sys.argv:
+            i = sys.argv.index('-max_payload')
+            big_truck_payload = int(sys.argv[i])
+        
+        if '-unload_time' in sys.argv:
+            i = sys.argv.index('-unload_time')
+            avg_unload_secs = int(sys.argv[i])
+
+        # get the data from google maps 
+        trips = DeliveryLogistics.GoogleMapsTripSetBuilder.build(google_api_key, customer_orders, distributionCenters)
+
+        # save the trips to a file if that's what the user wants
+        if '-o' in sys.argv:
+            i = sys.argv.index('-o')
+            f = sys.argv[i+1]
+            DeliveryLogistics.write_trips_to_json(trips, f)
+
+    elif '-from_file' in sys.argv:
+        i = sys.argv.index('-from_file')
+        f = sys.argv[i+1]
+        trips = DeliveryLogistics.read_trips_from_json(f)
     
-    # load the google API key
-    f = open(api_key_file,)
-    data = json.load(f)
-    google_api_key = data['google api key']
-    f.close()
-
-    # get the data from google maps 
-    #trips = DeliveryLogistics.GoogleMapsTripSetBuilder.build(google_api_key, customer_orders, distributionCenters)
-    #DeliveryLogistics.write_trips_to_json(trips, 'trips.json')
-
-    trips = DeliveryLogistics.read_trips_from_json('trips.json')
+    else:
+        raise ValueError('Expected either "-from_file" or "-from_google" as a command line arg.')
     
     # instantiate a route planning object
     planner = DeliveryLogistics.RoutePlanner(trips)
@@ -46,8 +83,8 @@ if __name__ == "__main__":
     print('Total Unload Time', round(routes['Total Unload Time']/3600,1), 'hours')
     print('Total Delivery Time', round(routes['Total Delivery Time']/3600,1), 'hours')
 
-    # display the routes in google maps
-    #for route in routes['Routes']:
-    #    DeliveryLogistics.open_route_in_browser(route['Delivery Locations'])
+    # display the routes in google maps via the browser
+    for route in routes['Routes']:
+        DeliveryLogistics.open_route_in_browser(route['Delivery Locations'])
     
 
